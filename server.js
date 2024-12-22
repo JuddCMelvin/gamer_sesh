cf49a9fafd2b7563e0f26f78eb2afc1512215bf8
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-12-22 08:48:32 -0500

    Added 404 page rout

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const express = require('express')
 const app = express()
 
 // Create a homepage route.
-app.get('/', function (req, res) {
+app.get('/', (req, res)  => {
     // This gets sent to the client
     // (your web browser most likely!)
     res.send('Hello world')
+})
+
+app.get('*', (req,res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 })
 
 // Listen for connections.
